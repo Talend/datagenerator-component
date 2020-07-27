93200ddc95794630f21c5be23cc147f9eb75551d
Thibaut Gourdel <tgourdel@talend.com>
2020-07-27 15:55:32 +0200

    Update Icon SVG

--- src/main/java/com/datagenerator/talend/components/package-info.java
+++ src/main/java/com/datagenerator/talend/components/package-info.java
@@ -1,7 +1,7 @@
 // this tells the framework in which family (group of components) and categories (UI grouping)
 // the components in the nested packages belong to
 @Components(family = "DataGenerator", categories = "Misc")
-@Icon(value = Icon.IconType.CUSTOM, custom = "DataGeneratorIcon")
+@Icon(value = Icon.IconType.CUSTOM, custom = "DataGenerator")
 package com.datagenerator.talend.components;
 
 import org.talend.sdk.component.api.component.Components;
